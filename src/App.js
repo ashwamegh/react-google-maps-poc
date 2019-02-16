158f1fb47eccfd5e05d14b2b74d47f12280af9f4
Shashank Shekhar <shashank7200@gmail.com>
2019-02-16 17:12:04 +0530

    added google map component to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleMapComponent from './components/googlemap/index';
 
 class App extends Component {
   render() {
@@ -20,6 +21,21 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <main>
+          <section>
+          <h2 className="section-header">Simple Google map with Marker and Circle</h2>
+          <div className="section-content">
+          <GoogleMapComponent 
+            isCircleShown
+            isMarkerShown
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GMAP_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          ></GoogleMapComponent>
+          </div>
+          </section>
+        </main>
       </div>
     );
   }
